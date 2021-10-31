76c2ff1fe8d57bd269afcc6b424ec4c191ae3333
Raudi <rbacallao@geocom.com.uy>
2021-10-31 14:35:11 -0300

    - Longest Vowel Chain

---
+++ lib/longest_vowel_chain.dart
@@ -0,0 +1,24 @@
+/*
+The vowel substrings in the word codewarriors are o,e,a,io. The longest of these has a length of 2. 
+Given a lowercase string that has alphabetic characters only (both vowels and consonants) and no spaces, 
+return the length of the longest vowel substring. Vowels are any of aeiou.
+*/
+const String vowels = 'aeiou';
+
+int solve(String s) {
+  int max = 0, count = 0;
+  s.split('').forEach((c) => (() {
+        if (vowels.contains(c)) {
+          count++;
+        } else {
+          max = count > max ? count : max;
+          count = 0;
+        }
+      }()));
+  return max;
+}
+
+void main() {
+  String value = 'codeoiwarriors ';
+  print(solve(value));
+}
