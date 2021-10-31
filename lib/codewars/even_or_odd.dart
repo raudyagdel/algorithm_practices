ba056010ff7ce7e85385324607c34672944e87a4
Raudi <rbacallao@geocom.com.uy>
2021-10-31 15:38:54 -0300

    - Event Or Odd Solution

---
+++ lib/codewars/even_or_odd.dart
@@ -0,0 +1,20 @@
+/*
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
+
+const word = ['Even', 'Odd'];
+
+extension Parity on int {
+  String parity() => this % 2 == 0 ? 'Even' : 'Odd';
+}
+
+String evenOrOdd_v1(int number) => number.isEven ? 'Even' : 'Odd';
+String evenOrOdd_v2(int number) => number.parity();
+String evenOrOdd_v3(int number) => word[number % 2];
+
+void main() {
+  int value = 2;
+  print(evenOrOdd_v1(value));
+  print(evenOrOdd_v2(value));
+  print(evenOrOdd_v3(value));
+}
