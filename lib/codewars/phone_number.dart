020781ded0b96af942f1ee253e1b6b15db6b02dc
Raudi <rbacallao@geocom.com.uy>
2022-05-02 12:19:20 -0300

    - Go Hello World Example - Optimized Phone Number

--- lib/codewars/phone_number.dart
+++ lib/codewars/phone_number.dart
@@ -1,9 +1,18 @@
-String createPhoneNumber(List numbers) {
-  String p = numbers.join('');
+/*
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+Don't forget the space after the closing parentheses!
+*/
+
+String createPhoneNumber_v1(List n) {
+  String p = n.join('');
   return '(${p.substring(0, 3)}) ${p.substring(3, 6)}-${p.substring(6)}';
 }
 
 void main() {
-  String value = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+  String value = createPhoneNumber_v1([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
   print(value);
 }
